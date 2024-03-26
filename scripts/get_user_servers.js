0b9f48ab8f1d51bb9cfa1339ad305c80fc5cf8a3
jamesm2002 <jm2004@hw.ac.uk>
2024-03-26 14:34:59 +0000

    homepage fixed

--- scripts/get_user_servers.js
+++ scripts/get_user_servers.js
@@ -20,4 +20,25 @@ async function view_user_servers(userid) {
     return result;
   }
 
+  async function list_servers(userid) {
+    try {
+        const formData = new FormData();
+        formData.append('userId', userid); 
+
+        const response = await fetch("https://softboxcollide.glitch.me/get_all_servers_with_user", {
+            method: "POST",
+            mode: "cors",
+            body: formData
+        });
+
+        const result = await response.json();
+
+        const serverIds = result.map(server => server.serverid);
+        return serverIds;
+    } catch (error) {
+        console.error("Error:", error);
+        return []; 
+    }
+}
+
   
