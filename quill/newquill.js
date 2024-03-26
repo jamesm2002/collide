01221a216b516658ceebd349f4bd76c1bb67eee6
jamesm2002 <jm2004@hw.ac.uk>
2024-03-26 15:54:31 +0000

    comments have been added

--- quill/newquill.js
+++ quill/newquill.js
@@ -1,17 +1,11 @@
-// will create a new file for user
-
 // saves and creates a document thread
 async function create_document(serverid, userid) {
     console.log("Adding document thread...");
 
     const threadBody = document.getElementById('threadBodyDocument').value.toString();
 
-    // Get the updated content from Quill editor
     const updatedContent = "<p></p>";
-    console.log("Updating document content");
 
-    // Add a new document thread to the database
-    console.log("Adding document thread...");
     const formData = new FormData();
     formData.append('title', 'Your Title');
     formData.append('body', threadBody);
@@ -19,11 +13,23 @@ async function create_document(serverid, userid) {
     formData.append('creatorId', userid);
     formData.append('docLink', updatedContent);
 
-    await fetch("https://softboxcollide.glitch.me/add_document_thread", {
-        method: "POST",
-        mode: "cors",
-        body: formData
-    });
+    try {
+        const response = await fetch("https://softboxcollide.glitch.me/add_document_thread", {
+            method: "POST",
+            mode: "cors",
+            body: formData
+        });
+
+        if (response.ok) {
+            alert("Document thread added successfully.");
+            
+            location.reload();
+        } else {
+            console.error("Failed to add document thread.");
+        }
+    } catch (error) {
+        console.error("Error:", error);
+    }
 
     console.log("Document thread added successfully.");
 }
